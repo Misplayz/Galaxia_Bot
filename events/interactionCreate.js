1c5367965e9728ac7221da45563b7283eb00bc59
Misplayz <98254147+Misplayz@users.noreply.github.com>
2024-05-30 11:19:53 +0700

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,62 +1,61 @@
-const { Events } = require('discord.js');
+const { Events, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
         if (interaction.isChatInputCommand()) {
-            // Process for commands entered through slash commands
             const command = interaction.client.commands.get(interaction.commandName);
+
             if (!command) {
                 console.error(`No command matching ${interaction.commandName} was found.`);
                 return;
             }
+
             try {
                 await command.execute(interaction);
             } catch (error) {
-                console.error(`Error executing ${interaction.commandName}`);
                 console.error(error);
+                if (interaction.replied || interaction.deferred) {
+                    await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+                } else {
+                    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+                }
             }
-        } else if (interaction.isMessageComponent() && interaction.message.components.length > 0) {
-            // Process for Message Components (including modals)
-            if (interaction.isButton()) {
-                // Process for buttons
-            } else if (interaction.isSelectMenu()) {
-                // Process for select menus
-            } else if (interaction.isModalSubmit()) {
-                // Check if the submitted modal is the introduction modal
-                if (interaction.customId === 'introduceModal') {
-                    const nickname = interaction.fields.getTextInputValue('nicknameInput');
-                    const age = interaction.fields.getTextInputValue('ageInput');
-                    const gender = interaction.fields.getTextInputValue('genderInput');
+        }
 
-                    const taggedUser = interaction.user.toString();
-                    const messageContent = `${taggedUser}`;
-                    const indEmbed = new EmbedBuilder()
-                        .setTitle('Introduction has been confirmed.✅')
-                        .setColor('#00FFFF')
-                        .addFields(
-                            { name: 'Nickname', value: nickname },
-                            { name: 'Age', value: age },
-                            { name: 'Gender', value: gender }
-                        )
-                        .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
-                        .setTimestamp();
+        if (interaction.isModalSubmit()) {
+            // Check if the submitted modal is the introduction modal
+            if (interaction.customId === 'introduceModal') {
+                const nickname = interaction.fields.getTextInputValue('nicknameInput');
+                const age = interaction.fields.getTextInputValue('ageInput');
+                const gender = interaction.fields.getTextInputValue('genderInput');
 
-                    const targetChannel = interaction.guild.channels.cache.get('1243918701749866630');
-                    if (targetChannel) {
-                        await targetChannel.send({ content: messageContent, embeds: [indEmbed] });
+                const taggedUser = interaction.user.toString();
+                const messageContent = `${taggedUser}`;
+                const indEmbed = new EmbedBuilder()
+                    .setTitle('Introduction has been confirmed.✅')
+                    .setColor('#00FFFF')
+                    .addFields(
+                        { name: 'Nickname', value: nickname },
+                        { name: 'Age', value: age },
+                        { name: 'Gender', value: gender }
+                    )
+                    .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
+                    .setTimestamp();
 
-                        // Add new role and remove old role
-                        const member = await interaction.guild.members.fetch(interaction.user);
-                        const newRole = interaction.guild.roles.cache.get('1243914187747627008');
-                        const oldRole = interaction.guild.roles.cache.get('1243913819580010536');
+                const targetChannel = interaction.guild.channels.cache.get('1243918701749866630');
+                if (targetChannel) {
+                    await targetChannel.send({ content: messageContent, embeds: [indEmbed] });
 
-                        if (newRole) await member.roles.add(newRole);
-                        if (oldRole) await member.roles.remove(oldRole);
+                    // Add new role and remove old role
+                    const member = await interaction.guild.members.fetch(interaction.user);
+                    const newRole = interaction.guild.roles.cache.get('1243914187747627008');
+                    const oldRole = interaction.guild.roles.cache.get('1243913819580010536');
 
-                    } else {
-                        console.log('Target channel not found.');
-                    }
+                    if (newRole) await member.roles.add(newRole);
+                    if (oldRole) await member.roles.remove(oldRole);
+                } else {
+                    console.log('Target channel not found.');
                 }
             }
         }
